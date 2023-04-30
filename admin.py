032f0e910b9d1e3beba6be986e50b8d5dddd39dc
Guilhem Duval <guilhem.duval@live.fr>
2023-04-30 11:03:35 +0200

    Improved addMateriel + Rough deleteMateriel

--- admin.py
+++ admin.py
@@ -84,15 +84,50 @@ def admin_addMateriel():
     # If the admin_id isn't empty, it means that the admin is connected
     if admin_id:
         try:
-            mydb = current_app.config['mydb']
-            mycursor = mydb.cursor()
-            print("inside admin_addMateriel  ")
-            tempQuery = '''INSERT INTO Materiel(type, modele, description, quantite, image, remarque) 
-            VALUES (%s, %s, %s, %s, %s, %s)'''
-            mycursor.execute(tempQuery, (str(request.form['type']), str(request.form['modele']), str(request.form['description']), str(request.form['image']), str(request.form['quantite']), str(request.form['remarque'])))
-            mydb.commit()
-            mycursor.close()
-            return redirect(url_for("admin.admin_manageMateriel"))
+            if request.form['type'] and request.form['modele'] and request.form['description']:
+                mydb = current_app.config['mydb']
+                mycursor = mydb.cursor()
+                print("inside admin_addMateriel  ")
+                if request.form['image']:
+                    tempQuery = '''INSERT INTO Materiel(type, modele, description, quantite, image, remarque) 
+                    VALUES (%s, %s, %s, %s, %s, %s)'''
+                    mycursor.execute(tempQuery, (str(request.form['type']), str(request.form['modele']), str(request.form['description']), str(request.form['image']), str(request.form['quantite']), str(request.form['remarque'])))
+                # Case when no image url is send
+                else:
+                    tempQuery = '''INSERT INTO Materiel(type, modele, description, quantite, remarque) 
+                    VALUES (%s, %s, %s, %s, %s)'''
+                    mycursor.execute(tempQuery, (str(request.form['type']), str(request.form['modele']), str(request.form['description']), str(request.form['quantite']), str(request.form['remarque'])))
+                mydb.commit()
+                mycursor.close()
+                return redirect(url_for("admin.admin_manageMateriel"))
+            else:
+                e = "Erreur : données manquantes pour insérer ce matériel"
+                return render_template("pages/admin.html", error=str(e))
+        except Exception as e: # If the query fails, render the template with a user-friendly error message
+            return render_template("pages/admin.html", error=str(e))
+    # If it's empty, it means that the admin isn't connected
+    # If so, redirect to the admin connection page
+    else:
+        return redirect(url_for("admin.admin_connexion"))
+
+@admin.route("/manageMateriel/deleteMateriel", methods=['POST'])
+def admin_deleteMateriel():
+    admin_id = session.get("admin_id")
+    # If the admin_id isn't empty, it means that the admin is connected
+    if admin_id:
+        try:
+            if request.form['id_materiel']:
+                mydb = current_app.config['mydb']
+                mycursor = mydb.cursor()
+                print("inside admin_deleteMateriel   ")
+                tempQuery = '''DELETE FROM Materiel WHERE id_materiel = %s;'''
+                mycursor.execute(tempQuery, (request.form['id_materiel'],))
+                mydb.commit()
+                mycursor.close()
+                return redirect(url_for("admin.admin_manageMateriel"))
+            else:
+                e = "Erreur : pas d'ID de matériel renseigné pour la suprression"
+                return render_template("pages/admin.html", error=str(e))
         except Exception as e: # If the query fails, render the template with a user-friendly error message
             return render_template("pages/admin.html", error=str(e))
     # If it's empty, it means that the admin isn't connected
